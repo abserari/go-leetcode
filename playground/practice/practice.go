6cb6f35f19047fdfbce5daf7f22d3387bebb7801
yhyddr <349037616@qq.com>
2019-04-04 00:48:38 +0800

    [practice] gobyexample

--- playground/practice/practice.go
+++ playground/practice/practice.go
@@ -1,18 +1,53 @@
 package main
 
-func sum(nums ...int){
-	fmt.Print(nums, " ")
-	total :=  0 
-	for _ , num := range nums{
-		total += num
+import (
+	"errors"
+	"fmt"
+)
+
+func f1(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("can't work with 42")
 	}
-	fmt.Println(total)
+
+	return arg + 3, nil
 }
 
-func main(){
-	sum(1,2)
-	sum(1,2,3)
+type argError struct {
+	arg  int
+	prob string
+}
 
-	nums := []int{1,2,3,4}
-	sum(nums...)
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+func f2(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg, "can't work with it"}
+	}
+	return arg + 3, nil
+}
+
+func main() {
+	for _, i := range []int{7, 42} {
+		if r, e := f1(i); e != nil {
+			fmt.Println("f1 failed:", e)
+		} else {
+			fmt.Println("f1 worked:", r)
+		}
+	}
+	for _, i := range []int{7, 42} {
+		if r, e := f2(i); e != nil {
+			fmt.Println("f2 failed:", e)
+		} else {
+			fmt.Println("f2 worked", r)
+		}
+	}
+
+	_, e := f2(42)
+	if ae, ok := e.(*argError); ok {
+		fmt.Println(ae.arg)
+		fmt.Println(ae.prob)
+	}
 }
