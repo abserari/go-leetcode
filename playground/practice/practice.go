041761da9adc6dae11f224643dae364bb94c3daf
yhyddr <349037616@qq.com>
2019-04-21 17:39:56 +0800

    make abser to playground

--- playground/practice/practice.go
+++ playground/practice/practice.go
@@ -1,53 +1,30 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"time"
 )
 
-func f1(arg int) (int, error) {
-	if arg == 42 {
-		return -1, errors.New("can't work with 42")
-	}
-
-	return arg + 3, nil
-}
-
-type argError struct {
-	arg  int
-	prob string
-}
-
-func (e *argError) Error() string {
-	return fmt.Sprintf("%d - %s", e.arg, e.prob)
-}
-
-func f2(arg int) (int, error) {
-	if arg == 42 {
-		return -1, &argError{arg, "can't work with it"}
-	}
-	return arg + 3, nil
-}
-
 func main() {
-	for _, i := range []int{7, 42} {
-		if r, e := f1(i); e != nil {
-			fmt.Println("f1 failed:", e)
-		} else {
-			fmt.Println("f1 worked:", r)
-		}
-	}
-	for _, i := range []int{7, 42} {
-		if r, e := f2(i); e != nil {
-			fmt.Println("f2 failed:", e)
-		} else {
-			fmt.Println("f2 worked", r)
-		}
-	}
 
-	_, e := f2(42)
-	if ae, ok := e.(*argError); ok {
-		fmt.Println(ae.arg)
-		fmt.Println(ae.prob)
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
 	}
 }
